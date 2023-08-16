1dbd3a2cca1ad91ff6e74406cd97badbbbe43cba
KiUkKim <zidh1@korea.ac.kr>
2023-08-16 23:52:39 +0900

    [ADD] DP_EXAMPLE BOJ_1010

---
+++ Samsung_SDS/src/DP_EX/BOJ_1010/BOJ_1010.java
@@ -0,0 +1,20 @@
+package DP_EX.BOJ_1010;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_1010 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+
+	}
+
+}
